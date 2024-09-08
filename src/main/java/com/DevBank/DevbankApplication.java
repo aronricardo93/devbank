42ac0fc57c08ab0925450115b3b66c898b920737
Aron Ricardo <aronricardo@hotmail.com>
2024-09-08 17:08:25 -0300

    Initial commit

---
+++ src/main/java/com/DevBank/DevbankApplication.java
@@ -0,0 +1,11 @@
+package com.DevBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+@SpringBootApplication
+public class DevbankApplication {
+
+	public static void main(String[] args) {SpringApplication.run(DevbankApplication.class, args);}
+}
---
+++ src/test/java/com/DevBank/DevbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
