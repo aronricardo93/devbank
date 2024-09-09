e78cbf5a087eec9824866b887746bc8eba5af805
Aron Ricardo <aronricardo@hotmail.com>
2024-09-09 00:45:46 -0300

    [REFACTOR] - removing redundancy from feature and news

--- src/main/java/com/DevBank/domain/models/Feature.java
+++ src/main/java/com/DevBank/domain/models/Feature.java
@@ -3,22 +3,4 @@ package com.DevBank.domain.models;
 import jakarta.persistence.*;
 
 @Entity(name = "tbFeature")
-public class Feature {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String icon;
-    private String description;
-
-    public Long getId() {return id;}
-
-    public void setId(Long id) {this.id = id;}
-
-    public String getIcon() {return icon;}
-
-    public void setIcon(String icon) {this.icon = icon;}
-
-    public String getDescription() {return description;}
-
-    public void setDescription(String description) {this.description = description;}
-}
+public class Feature extends baseItem{}
--- src/main/java/com/DevBank/domain/models/News.java
+++ src/main/java/com/DevBank/domain/models/News.java
@@ -6,22 +6,4 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity(name = "tbNews")
-public class News {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String icon;
-    private String description;
-
-    public Long getId() {return id;}
-
-    public void setId(Long id) {this.id = id;}
-
-    public String getIcon() {return icon;}
-
-    public void setIcon(String icon) {this.icon = icon;}
-
-    public String getDescription() {return description;}
-
-    public void setDescription(String description) {this.description = description;}
-}
+public class News extends baseItem{}
