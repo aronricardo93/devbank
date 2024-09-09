2420a1a1c24e5aedac94d3390d8dd2959d14a0e7
Aron Ricardo <aronricardo@hotmail.com>
2024-09-09 00:32:00 -0300

    [CHORE] - getters, setters and JPA anotations

--- src/main/java/com/DevBank/domain/models/Feature.java
+++ src/main/java/com/DevBank/domain/models/Feature.java
@@ -1,7 +1,24 @@
 package com.DevBank.domain.models;
 
+import jakarta.persistence.*;
+
+@Entity(name = "tbFeature")
 public class Feature {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String icon;
     private String description;
+
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getIcon() {return icon;}
+
+    public void setIcon(String icon) {this.icon = icon;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription(String description) {this.description = description;}
 }
