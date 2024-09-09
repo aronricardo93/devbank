b29337e010afd3ba675d9fde3a3c0e4bf6cd19e8
Aron Ricardo <aronricardo@hotmail.com>
2024-09-09 01:44:33 -0300

    [STYLE] - rename entity

--- src/main/java/com/DevBank/domain/models/Card.java
+++ src/main/java/com/DevBank/domain/models/Card.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
-@Entity
+@Entity(name = "tbCard")
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
